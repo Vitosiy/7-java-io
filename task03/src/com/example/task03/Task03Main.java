0724907eccd999f544594df954a9617b82874265
Виталий Вересников <Vitosiy21@yandex.ru>
2020-11-29 18:10:49 +0500

    Task03 [Veresnikov]

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,6 +3,8 @@ package com.example.task03;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
@@ -15,7 +17,17 @@ public class Task03Main {
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null) {
+            throw new IllegalArgumentException("Файл не найден");
+        }
+        if (charset == null) {
+            throw new IllegalArgumentException("Нет кодировки");
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, charset);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (inputStreamReader.ready()) {
+            stringBuilder.appendCodePoint(inputStreamReader.read());
+        }
+        return stringBuilder.toString();
     }
 }
